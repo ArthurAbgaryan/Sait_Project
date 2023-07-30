e051b7f025c68397ab423995f493126568ce2c67
Arthur <abgaryanarthurelina@gmail.com>
2023-07-30 13:35:15 +0300

    Редактирование в основной ветке и в pandasпопытка 2

--- practice_django/pandas_app/pandas1.py
+++ practice_django/pandas_app/pandas1.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-kas22r = pd.DataFrame(
+kas22r1213 = pd.DataFrame(
     {"age": [30,20,23],
      "name": ['Jon','Katrin','Battler'],
      "hobby": ['avto','spining','basketbool']}
 )
 
-print(kas22r)
+print(kas22r1213)
